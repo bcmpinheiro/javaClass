34cf6571c591ab30d96fa1077e4507a021382dfd
bcmpinheiro <bcmpinheiro@gmail.com>
2021-05-18 21:54:11 -0300

    Primeiros Exercicios

---
+++ aula06/Argumentos.java
@@ -0,0 +1,5 @@
+class Argumentos {
+  public static void main(String[] args) {
+    System.out.println("Voce digitou " + args[0]);
+  }
+}
---
+++ aula06/labs/Argumentos2.java
@@ -0,0 +1,5 @@
+class Argumentos2 {
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo " + args[0]);
+  }
+}
