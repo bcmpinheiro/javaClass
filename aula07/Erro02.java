23ebdb7aaa64aec1a2355df8eecc8c2189c4cb95
bcmpinheiro <bcmpinheiro@gmail.com>
2021-05-18 23:21:02 -0300

    Erros Comuns

---
+++ aula07/Erro02.java
@@ -0,0 +1,6 @@
+class Erro02 {
+  public static void main(String[] args) {
+    int count = 0;
+    System.out.println("Você digitou:" + count);
+  }
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,5 @@
+class Erro03 {
+  public static void Main(String[] args) {
+    System.out.println("Voce digitou: ");
+  }
+}
